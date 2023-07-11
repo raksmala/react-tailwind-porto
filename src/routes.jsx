2c1a4ee89c4c9459927cac9a9099d0e522dc1df4
raksmala <raksmala@gmail.com>
2023-07-11 11:00:19 +0700

    init

---
+++ src/routes.jsx
@@ -0,0 +1,32 @@
+import { Home, Profile, SignIn, SignUp } from "@/pages";
+import {
+  HomeIcon,
+  UserCircleIcon,
+  ArrowRightOnRectangleIcon,
+  UserPlusIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/solid";
+
+export const routes = [
+  {
+    icon: HomeIcon,
+    name: "home",
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    icon: UserCircleIcon,
+    name: "profile",
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    icon: DocumentTextIcon,
+    name: "Portofolio",
+    href: "https://github.com/raksmala",
+    target: "_blank",
+    element: "",
+  },
+];
+
+export default routes;
